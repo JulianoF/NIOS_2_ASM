81866317bae11cc32a5dbf00674f6c5a8398566b
Juliano Falotico <juliano1@live.ca>
2023-10-04 21:04:53 -0400

    Starting I/O Manip

---
+++ PONG_REV0.01.s
@@ -0,0 +1,48 @@
+.equ PIXEL_BUFFER_START, 0x08000000
+.equ PIXEL_BUFFER_END, 0x08080000
+.equ CHAR_BUFFER, 0x09000000
+.equ BUTTON_BUFFER, 0x10000050
+
+.global _start
+_start:
+
+INIT:							/*EVERYTHING THAT ONLY RUNS ON STARTUP HERE*/
+ movia r2,PIXEL_BUFFER_START		
+ movia r3,CHAR_BUFFER
+ movia r6,PIXEL_BUFFER_END
+ movia r18,BUTTON_BUFFER
+ 
+ mov r4,r0			/*CLEAR CURRENT WORKING REGISTERS*/
+ mov r5,r0
+ 
+ 
+ mov r7,r0
+ mov r8,r0
+ mov r9,r0
+ mov r10,r0
+ 
+ movui r4,0x0000 /*Black Pixel MOVED HERE FOR TEST*/
+ 
+INIT_VGA:
+ beq r2,r6,END_INIT_VGA
+ sthio r4,(r2)
+ addi r2,r2,2
+ addi r5,r5,1
+ br INIT_VGA
+END_INIT_VGA:
+ movia r2,PIXEL_BUFFER_START
+ mov r4,r0
+ mov r5,r0
+ br GAMELOOP
+
+
+GAMELOOP: /*MAIN GAME LOOP*/
+ call DRAW
+ ldhio r19,(r18) /*check is button is pressed by value loaded into r19*/
+ beq r0,r19, INIT_VGA /* IF BUTTON NOT PRESSED PAINT VGA BLACK*/
+ movui r4,0xffff /*White Pixel*/
+ bne r0,r19 ,INIT_VGA /* IF BUTTON IS PRESSED PAINT VGA WHITE*/
+ jmpi GAMELOOP
+	
+DRAW: /*Not implemented*/
+ ret
